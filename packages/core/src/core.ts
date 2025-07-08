8fdaf189f790c34c5b9409a0913b1cf393e014a8
Markus Ecker <markus.ecker@gmail.com>
2025-07-09 00:07:16 +0200

    update core

--- packages/core/src/core.ts
+++ packages/core/src/core.ts
@@ -1,22 +1,66 @@
 import { randomUUID } from "@copilotkit/shared";
 import type { MaybePromise } from "@copilotkit/shared";
 import { z } from "zod";
+import { logger } from "@copilotkit/shared";
+import type { HttpAgent } from "@ag-ui/client";
 
-interface CopilotContext {
+export interface CopilotContext {
   description: string;
   value: string;
 }
 
-interface CopilotTool<T = unknown> {
+export type CopilotToolRenderState<T> =
+  | {
+      status: "inProgress";
+      args: Partial<T>;
+      result: undefined;
+    }
+  | {
+      status: "executing";
+      args: T;
+      result: undefined;
+    }
+  | {
+      status: "complete";
+      args: T;
+      result: any;
+    };
+
+export interface CopilotTool<T = unknown> {
   name: string;
+  description?: string;
   schema: z.ZodSchema<T>;
   handler?: (params: T) => MaybePromise<unknown>;
-  render?: (params: T) => unknown;
+  render?: (state: CopilotToolRenderState<T>) => unknown;
+}
+
+export interface CopilotKitCoreConfig {
+  runtimeUrl: string;
+  headers: Record<string, string>;
+  properties: Record<string, unknown>;
 }
 
 export class CopilotKitCore {
   context: Record<string, CopilotContext> = {};
   tools: Record<string, CopilotTool<unknown>> = {};
+  headers: Record<string, string>;
+  runtimeUrl: string;
+  properties: Record<string, unknown>;
+
+  // https://chatgpt.com/share/686d8cb9-9284-800b-8a12-a03911d50f12
+  agents: Promise<Record<string, HttpAgent>>;
+
+  constructor({ headers, runtimeUrl, properties }: CopilotKitCoreConfig) {
+    this.headers = headers;
+    this.runtimeUrl = runtimeUrl;
+    this.properties = properties;
+    this.agents = this.fetchAgents();
+  }
+
+  private async fetchAgents(): Promise<Record<string, HttpAgent>> {
+    // TODO: hook into agents: before request, change tools etc.
+    throw new Error("Not implemented");
+  }
 
   addContext({ description, value }: CopilotContext): string {
     const id = randomUUID();
@@ -29,11 +73,27 @@ export class CopilotKitCore {
   }
 
   addTool<T = unknown>(tool: CopilotTool<T>) {
-    // TODO: warn
-    this.tools[tool.name] = tool as CopilotTool<unknown>;
+    const id = randomUUID();
+    for (const t of Object.values(this.tools)) {
+      if (t.name === tool.name) {
+        logger.warn(`Tool already exists: '${tool.name}', skipping...`);
+        return id;
+      }
+    }
+
+    this.tools[id] = tool as CopilotTool<unknown>;
+    return id;
   }
 
-  removeTool(name: string) {
-    delete this.tools[name];
+  removeTool(id: string) {
+    delete this.tools[id];
+  }
+
+  setHeaders(headers: Record<string, string>) {
+    this.headers = headers;
+  }
+
+  setProperties(properties: Record<string, unknown>) {
+    this.properties = properties;
   }
 }
